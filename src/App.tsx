85135f937e3679bdda150191d475153a9298ec1d
Adrian Lupu <ansharai@yahoo.com>
2020-09-17 10:31:33 +0300

    added redux, react-redux types to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from 'react'
 import {Home} from './home/Home'
+import {Provider} from 'react-redux';
+import {rootReducers} from './reducers'
+import {createStore} from 'redux';
+const store= createStore(rootReducers)
 
 function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <Home/>
+            </div>
+        </Provider>
+    )
 }
 
-export default App;
+export default App
